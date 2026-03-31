4fd96468a8918924d7b8bd3afb2d0f81fca72c0a
dhakchianandan <dhakchianandan@gmail.com>
2026-03-31 23:16:11 +0530

    feat: vercel deploy

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,9 +3,11 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { nitro } from 'nitro/vite';
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
+  base: process.env.VITE_BASE_PATH || '/',
+  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
   ssr: {
     noExternal: ['@convex-dev/better-auth'],
   },
